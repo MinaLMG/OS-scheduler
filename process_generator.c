0f0ea32f82703e76f158f2eb3455880277620f88
MinaLMG <beandro6@gmail.com>
2021-12-09 00:07:18 +0200

    used Queue struct , handled agorithm part , initiated scheduler

--- process_generator.c
+++ process_generator.c
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
         // Program exits if the file pointer returns NULL.
         exit(1);
     }
-    create();
+    struct Queue *q = create();
     while (!feof(fptr))
     {
-        // person * myperson = (person *) malloc(sizeof(person)); 
+        // person * myperson = (person *) malloc(sizeof(person));
         // struct Process p1;
         struct Process *p = (struct Process *)malloc(sizeof(struct Process));
         char firstString[256];
@@ -56,22 +56,46 @@ int main(int argc, char *argv[])
         p->remaining_time = p->run_time;
         p->finish_time = -1;
         // printProcess(p);
-        insert_by_priority(p);
+
+        insert_by_priority_arrival_time(q, p);
         // printProcess( pri_que[0]);
         // printf("after a loop in file:\n");
         // display_pqueue();
     }
     // printProcess( pri_que[1]);
-    display_pqueue();
+    display_queue(q);
+    /*for (int i = 0; i < 6; i++)
+    {
+        struct Process *temp = dequeue(q);
+        printProcess(temp);
+        display_queue(q);
+    }*/
     fclose(fptr);
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
     int algorithm;
-    printf("enter the hoped algorithm number:");
+    printf("enter the hoped algorithm number\n1 for non-preemptive HPF\n2 for SRTN\n3 for round-robin :\n ");
     scanf("%d", &algorithm);
+    while (algorithm > 3 || algorithm < 1)
+    {
+        printf("you entered invalid number \n1 for non-preemptive HPF\n2 for SRTN\n3 for round-robin :\n ");
+        scanf("%d", &algorithm);
+    }
+    int rr = -1;
+    if (algorithm == 3)
+    {
+        printf("enter the quantum value\n ");
+        scanf("%d", &rr);
+        while (rr < 0)
+        {
+            printf("enter positive value for quantum\n ");
+            scanf("%d", &rr);
+        }
+    }
     printf("Value of algorithm =%d \n", algorithm);
+
     // 3. Initiate and create the scheduler and clock processes.
 
-    /*** initialixing clock***/
+    /*** initializing clock***/
     int clockId = fork();
     // printf("clokid= %d \n ",clockId);
     if (clockId == -1)
@@ -99,22 +123,33 @@ int main(int argc, char *argv[])
         /*** implemented in header ****/
 
         // 6. Send the information to the scheduler at the appropriate time.
-
-        // 7. Clear clock resources
-        destroyClk(true);
+        int schedulerId = fork();
+        if (schedulerId == -1)
+        {
+            printf("error in forking\n");
+        }
+        else if (schedulerId == 0)
+        {
+            char *argv[] = {"./schedular.out", NULL};
+            execv(argv[0], argv);
+        }
+        else
+        {
+            // 7. Clear clock resources
+            destroyClk(true);
+        }
     }
 }
 
 void clearResources(int signum)
 {
-    //TODO Clears all resources in case of interruption
+    // TODO Clears all resources in case of interruption
 }
 
 /* Driver program to test above function */
 /*int main()
 {
     struct Node *start = NULL;
- 
     // Create and print an int linked list
     unsigned int_size = sizeof(int);
     int arr[] = {10, 20, 30, 40, 50}, i;
@@ -122,7 +157,6 @@ void clearResources(int signum)
        push(&start, &arr[i], int_size);
     printf("Created integer linked list is \n");
     printList(start, printInt);
- 
     // Create and print a float linked list
     unsigned float_size = sizeof(float);
     start = NULL;
@@ -131,6 +165,5 @@ void clearResources(int signum)
        push(&start, &arr2[i], float_size);
     printf("\n\nCreated float linked list is \n");
     printList(start, printFloat);
- 
     return 0;
 }*/
