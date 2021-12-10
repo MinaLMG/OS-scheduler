5df57858f164fa4e0e6edae4462de6cbc68d721e
MinaLMG <beandro6@gmail.com>
2021-12-10 21:30:37 +0200

    modified input types

--- scheduler.c
+++ scheduler.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
     struct msgProcessBuff message;
     struct msgIntBuff message2;
     /* receive the value of algorithm */
-    receiveIntValue(fromGenToSchAlg, 550, algorithm, &message2);
+    receiveIntValue(fromGenToSchAlg, 550, &algorithm, &message2);
     printf("\nAlgorithm received: %d\n", message2.val);
     // algorithm=(int)message2.val;
     printf("algorithm = %d \n",algorithm);
     if (algorithm == 3)
     {
-        receiveIntValue(fromGenToSchAlg, 550, rr, &message2);
+        receiveIntValue(fromGenToSchAlg, 550, &rr, &message2);
         printf("\n rr received: %d\n", message2.val);
         rr=message2.val;
     }
@@ -27,11 +27,11 @@ int main(int argc, char *argv[])
     while (1)
     {
         // printf("i'm here \n");
-        receiveProcessValue(fromGenToSchPro, 500, to_receive, &message);
+        receiveProcessValue(fromGenToSchPro, 500, &to_receive, &message);
         printf("Process received : \n");
         to_receive=message.p;
-        // printProcess(&to_receive);
-        printProcess(&message.p);
+        printProcess(&to_receive);
+        // printProcess(&message.p);
     }
 
     destroyClk(true);
