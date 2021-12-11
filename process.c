27f7d799a940752896ad177a33e50c633f159f7c
MinaLMG <beandro6@gmail.com>
2021-12-11 15:59:10 +0200

    initially implemented process

--- process.c
+++ process.c
@@ -6,11 +6,17 @@ int remainingtime;
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    printf("fucken project \n");
+    key_t from_scheduler_to_process;
+    struct msgIntBuff * message;
+    int remainingtime;
+    receiveIntValue(from_scheduler_to_process,getpid(),&remainingtime,message);
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
     while (remainingtime > 0)
     {
+        printf("I'm inside the process , remaining time = %d \n",remainingtime);
+        receiveIntValue(from_scheduler_to_process,getpid(),&remainingtime,message);
         // remainingtime = ??;
     }
     
