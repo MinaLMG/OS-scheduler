e80c4a4c75030b0890db04b564426a8aa0df06b8
MinaLMG <beandro6@gmail.com>
2021-12-11 20:14:04 +0200

    built files

--- scheduler.c
+++ scheduler.c
@@ -28,27 +28,27 @@ int main(int argc, char *argv[])
     currentProcess->address = -1;
     // printf(" address = %d \n", (int)(currentProcess->address));
     int last_process = 0;
-    while (last_process == 0)
+    while (1)
     {
         struct Process *to_receive = (struct Process *)malloc(sizeof(struct Process));
-        printf("i'm here \n");
-        printf(" address = %d \n", currentProcess->address);
+        // printf("i'm here \n");
+        // printf(" address = %d \n", currentProcess->address);
         receiveProcessValue(fromGenToSchPro, 500, to_receive, &message);
         printf("Process received : \n");
         *to_receive = message.p;
         printProcess(to_receive);
         if (to_receive->last_process == 1)
             last_process = 1;
-        printf(" address = %d \n", currentProcess->address);
+        // printf(" address = %d \n", currentProcess->address);
         // printProcess(&message.p);
         if (to_receive->null != 100)
         {
-            printf("entered after receiving vallid process \n");
+            // printf("entered after receiving vallid process \n");
             if (algorithm == 1)
             {
                 // printf("displaying scheduler queue before\n");
                 insert_by_priority(Q, to_receive, 'p');
-                printf(" address = %d \n", currentProcess->address);
+                // printf(" address = %d \n", currentProcess->address);
                 // printf("displaying scheduler queue \n");
                 // display_queue(Q);
                 // printf("displaying scheduler queue after\n");
@@ -61,10 +61,10 @@ int main(int argc, char *argv[])
             }
         }
         printf("passed nullity check \n");
-        printProcess(to_receive);
-        printf(" address = %d \n", currentProcess->address);
+        // printProcess(to_receive);
+        // printf(" address = %d \n", currentProcess->address);
         printProcess(currentProcess);
-        printf("passed nullity check \n");
+        // printf("passed nullity check \n");
         if (currentProcess->address == -1)
         {
             struct Process *temp;
@@ -83,32 +83,47 @@ int main(int argc, char *argv[])
             }
             currentProcess->address = process_id;
             printProcess(currentProcess);
-            printf("new process id = %d \n", currentProcess->address);
+            // printf("new process id = %d \n", currentProcess->address);
             key_t from_scheduler_to_process;
             struct msgIntBuff *int_message = (struct msgIntBuff *)malloc(sizeof(struct msgIntBuff));
-            printf("%d \n", currentProcess->remaining_time);
+            // printf("%d \n", currentProcess->remaining_time);
             sendIntMesssage(from_scheduler_to_process, currentProcess->address, getpid(), currentProcess->remaining_time, int_message);
-            printf("message to send : %d %d %d %d %d\n", from_scheduler_to_process, currentProcess->address, getpid(), currentProcess->remaining_time, int_message->val);
+            // printf("message to send : %d %d %d %d %d\n", from_scheduler_to_process, currentProcess->address, getpid(), currentProcess->remaining_time, int_message->val);
             // printProcess(currentProcess);
             printf("\nMessage sent from scheduler at time %d : %d \n", getClk(), int_message->val);
             currentProcess->remaining_time--;
+            if (currentProcess->remaining_time == 0)
+            {
+                currentProcess = (struct Process *)malloc(sizeof(struct Process));
+                currentProcess->address = -1;
+            }
             // raise(SIGSTOP);
         }
         else
         {
-            printf("entering else condition \n");
+            // printf("entering else condition \n");
             // kill(currentProcess->address, SIGCONT);
             key_t from_scheduler_to_process;
             struct msgIntBuff *int_message = (struct msgIntBuff *)malloc(sizeof(struct msgIntBuff));
             sendIntMesssage(from_scheduler_to_process, currentProcess->address, getpid(), currentProcess->remaining_time, int_message);
-            printf("message to send : %d %d %d %d %d\n", from_scheduler_to_process, currentProcess->address, getpid(), currentProcess->remaining_time, int_message->val);
+            // printf("message to send : %d %d %d %d %d\n", from_scheduler_to_process, currentProcess->address, getpid(), currentProcess->remaining_time, int_message->val);
             // printProcess(currentProcess);
             // printf("%d \n",currentProcess->remaining_time);
             printf("\nMessage sent from scheduler at time %d : %d \n", getClk(), (currentProcess->remaining_time));
             currentProcess->remaining_time--;
+            if (currentProcess->remaining_time == 0)
+            {
+                currentProcess = (struct Process *)malloc(sizeof(struct Process));
+                currentProcess->address = -1;
+                if (currentProcess->last_process == 1)
+                    break;
+            }
         }
-        printf(" address = %d \n", currentProcess->address);
+        // printf(" address = %d \n", currentProcess->address);
     }
-
+    // key_t from_scheduler_to_generator;
+    // struct msgIntBuff *int_message = (struct msgIntBuff *)malloc(sizeof(struct msgIntBuff));
+    // sendIntMesssage(from_scheduler_to_generator, getppid(), getpid(), 1, int_message);
+    // printf("\nMessage sent from generator to scheduler at time %d : %d \n", getClk(),int_message->val);
     destroyClk(true);
 }
