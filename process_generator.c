4f10462f25f744996e1acb3599e0bd2359dd0cc2
MinaLMG <beandro6@gmail.com>
2021-12-18 03:55:16 +0200

    sent signal alarm for SRTN

--- process_generator.c
+++ process_generator.c
@@ -174,6 +174,9 @@ int main(int argc, char *argv[])
                     key_t fromGenToSchPro;
                     struct msgProcessBuff message;
                     sendProcessMesssage(fromGenToSchPro, 500, getpid(), *to_send, &message);
+                    if(algorithm==2){
+                        kill(schedulerId,SIGALRM);
+                    }
                     printf("\nProcess sent at time %d:\n", x);
                     printProcess(&message.p);
                     to_send = dequeue(q);
